813ff7cc3ff64c79a420a03357b5ad7194d8bf1b
zaur bersanov <storeborz@mail.ru>
2025-08-22 23:22:12 +0300

    Добавлен htaccess

--- components/CourseDetails/index.jsx
+++ components/CourseDetails/index.jsx
@@ -1,14 +1,10 @@
-"use client"
-import { useState } from "react"
-import { Dialog } from "@/components/ui/dialog"
+
 import { Separator } from "@/components/ui/separator"
 import { SchoolCoursesAccordion } from "@/components/SchoolCoursesAccordion"
-import { EnrollDialog } from "./EnrollDialog"
 import { CourseInfo } from "./CourseInfo"
 import { CourseSections } from "./CourseSections"
 
 export function CourseDetails({ course, details }) {
-  const [open, setOpen] = useState(false)
 
   return (
     <div className="mt-25 mb-10">
@@ -16,10 +12,15 @@ export function CourseDetails({ course, details }) {
         <CourseInfo course={course} />
         <Separator className="my-6 bg-gray-700" />
         <CourseSections sections={details.sections} />
-        <div className="text-center mt-8 md:mt-10">
-          <Dialog open={open} onOpenChange={setOpen}>
-            <EnrollDialog open={open} setOpen={setOpen} defaultCourse={course.title} />
-          </Dialog>
+        <div className="text-center mt-10 md:mt-12">
+          <a
+            href={"https://wa.me/79899306665"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-6 py-4 md:px-8 md:py-6 text-base md:text-lg font-semibold border border-white rounded-xl cursor-pointer hover:text-red-500 hover:border-current transition-colors duration-300"
+          >
+            Записаться
+          </a>
         </div>
       </div>
 
